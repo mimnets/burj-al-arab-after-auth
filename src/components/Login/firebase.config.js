369a551649b7206736855411edb1c5df2bf9db02
MiMNets <mdmonirulislammonir@gmail.com>
2020-09-30 00:51:48 +0600

    error on import to UserContext, and solved

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAsihSADFnF_FoZ6je8p1W9lilyqX7cHWQ",
-    authDomain: "burj-al-arab.firebaseapp.com",
-    databaseURL: "https://burj-al-arab.firebaseio.com",
-    projectId: "burj-al-arab",
-    storageBucket: "burj-al-arab.appspot.com",
-    messagingSenderId: "244522202387",
-    appId: "1:244522202387:web:f3801d0588387140e1d6e6"
-  };
-  export default firebaseConfig;
+  apiKey: "AIzaSyAUBcS8Pcxug18CtZjqwYxNydhOUjaV0RA",
+  authDomain: "ema-john-simple-projects.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-projects.firebaseio.com",
+  projectId: "ema-john-simple-projects",
+  storageBucket: "ema-john-simple-projects.appspot.com",
+  messagingSenderId: "214787728532",
+  appId: "1:214787728532:web:bfe052f16eb2d74b894081"
+};
+
+export default firebaseConfig;
